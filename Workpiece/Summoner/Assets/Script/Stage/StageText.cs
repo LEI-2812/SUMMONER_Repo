de4cf807f8b0a42cd555e39b7cd6dffe0ad3d47b
rud_Island <100767269+rudIsland@users.noreply.github.com>
2024-11-13 09:09:22 +0900

    이어하기 스테이지 이름 추가
    이어하기로 Alert창에 해당 스테이지 이름 넣었음.
    +) Text Pro가 한글이 깨지는 글자가 있어서 그냥 Text로 바꿈

--- Workpiece/Summoner/Assets/Script/Stage/StageText.cs
+++ Workpiece/Summoner/Assets/Script/Stage/StageText.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class StageText : MonoBehaviour
 {
-    public TMP_Text stageText; // 표시할 TextMeshPro 컴포넌트
+    public Text stageText; // 표시할 TextMeshPro 컴포넌트
 
     void Start()
     {
@@ -13,8 +13,39 @@ public class StageText : MonoBehaviour
         int savedStageValue = PlayerPrefs.GetInt("savedStage", 0); // 기본값 0
 
         // 패널에 savedStage 값을 표시
-        stageText.text = "<b>[" + savedStageValue.ToString() + "스테이지 : 임시 스테이지]</b>" +
-                            "<br>여기부터 진행하시겠습니까?";
+        stageText.text = "<b>[" + savedStageValue.ToString() + " 스테이지 : " + ShowTextSavedStage(savedStageValue) + "]</b>" +
+                 "\n여기부터 진행하시겠습니까?";
         // 나중에 스테이지 이름들도 정해지면 각 단계에 맞는 스테이지명을 넣어버리면 되지 않을까..?
+    }
+
+    private string ShowTextSavedStage(int stageNum)
+    {
+        string stageName = "";
+        switch (stageNum)
+        {
+            case 1:
+                stageName = "광활한 브리뉴 평원 동쪽";
+                break;
+            case 2:
+                stageName = "광활한 브리뉴 평원 서쪽";
+                break;
+            case 3:
+                stageName = "정령 숲지대의 왼편";
+                break;
+            case 4:
+                stageName = "정령 숲지대의 오른편";
+                break;
+            case 5:
+                stageName = "중간계의 오염지대";
+                break;
+            case 6:
+                stageName = "화염구의 무덤";
+                break;
+            case 7:
+                stageName = "다크 드래곤의 둥지";
+                break;
+        }
+
+        return stageName;
     }
 }
